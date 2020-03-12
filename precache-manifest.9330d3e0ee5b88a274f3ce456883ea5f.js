161e8e5fa1012f5c956a6551f4b7cbb9a7f2f393
NSarg7 <NSarg7@users.noreply.github.com>
2020-03-12 21:58:32 +0400

    Updates

---
+++ precache-manifest.9330d3e0ee5b88a274f3ce456883ea5f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24a10c6106c48cd443b761865fa6e8b6",
+    "url": "/mate-checker/index.html"
+  },
+  {
+    "revision": "203d140a4e0a4c10752c",
+    "url": "/mate-checker/static/css/main.df083a90.chunk.css"
+  },
+  {
+    "revision": "cba9e5d194bc7c8f3ca4",
+    "url": "/mate-checker/static/js/2.8fc772b7.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/mate-checker/static/js/2.8fc772b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "203d140a4e0a4c10752c",
+    "url": "/mate-checker/static/js/main.9c986212.chunk.js"
+  },
+  {
+    "revision": "25e28551c47d8330f4c4",
+    "url": "/mate-checker/static/js/runtime-main.1bb9adab.js"
+  },
+  {
+    "revision": "749c04188323133e511b5ce6ed5ad40b",
+    "url": "/mate-checker/static/media/back.749c0418.jpg"
+  },
+  {
+    "revision": "873d60b5bf81da806feccfe751c6c5c6",
+    "url": "/mate-checker/static/media/board1.873d60b5.png"
+  },
+  {
+    "revision": "5dc03ca6df9b31884553864da2a8a6c7",
+    "url": "/mate-checker/static/media/elph.5dc03ca6.png"
+  },
+  {
+    "revision": "83fbc674594abbf635860d605c270b9f",
+    "url": "/mate-checker/static/media/horse.83fbc674.png"
+  },
+  {
+    "revision": "6473958083772e46bff1eca07a0d39d7",
+    "url": "/mate-checker/static/media/king-black.64739580.png"
+  },
+  {
+    "revision": "35d03b8c454c14a6c5e7e7e840782336",
+    "url": "/mate-checker/static/media/king-white.35d03b8c.png"
+  },
+  {
+    "revision": "90314d4801d0fcd90ab5590f72b3febc",
+    "url": "/mate-checker/static/media/queen.90314d48.png"
+  }
+]);
